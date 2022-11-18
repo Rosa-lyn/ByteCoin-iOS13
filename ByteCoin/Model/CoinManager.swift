5107d9bd3bb7dc36ea69c54f80e9b48f37964c5c
Rosalyn Land <roz.s.land@gmail.com>
2022-11-18 10:43:31 +0000

    Make api request and print data

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -16,6 +16,24 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-        print(currency)
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+
+    func performRequest(with urlString: String) {
+        print("performing request with \(urlString)")
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print(error)
+                }
+
+                if let data = data {
+                    print(String(data: data, encoding: .utf8))
+                }
+            }
+            task.resume()
+        }
     }
 }
