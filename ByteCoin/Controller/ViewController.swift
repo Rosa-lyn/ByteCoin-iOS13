8edec2669663410d186dd9855c5b0f30eb6f589d
Rosalyn Land <roz.s.land@gmail.com>
2022-11-18 11:58:35 +0000

    Refactor to use extensions

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
-
+class ViewController: UIViewController {
 
     @IBOutlet weak var bitCoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -23,6 +22,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.delegate = self
         coinManager.delegate = self
     }
+}
+
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
 
     // number of columns
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -32,6 +36,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
+}
+
+// MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate {
 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
@@ -41,6 +50,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         let selectedCurrency = coinManager.currencyArray[row]
         coinManager.getCoinPrice(for: selectedCurrency)
     }
+}
+
+// MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
 
     func didUpdateCoinPrice(price: String, currency: String) {
         DispatchQueue.main.async {
@@ -53,4 +67,3 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         print(error)
     }
 }
-
